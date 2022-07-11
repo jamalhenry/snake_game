bfbb28d59140f98743096120c84ba25b42abd58c
Jamal Henry <Jamaljhenry@gmail.com>
2022-07-11 14:06:36 -0500

    Updated the game to keep a high score by using file paths.

--- scoreboard.py
+++ scoreboard.py
@@ -3,13 +3,15 @@ from turtle import Turtle
 ALIGNMENT = "center"
 FONT = ("Arial", 24, "normal")
 
+
 #Class that controls the scoreboard based off the amount off food the snake eats
 class Scoreboard(Turtle):
 
     def __init__(self):
         super().__init__()
         self.score = 0
-        self.high_score = 0
+        with open("data.txt") as data:
+            self.high_score = int(data.read())
         self.penup()
         self.color("white")
         self.goto(0, 270)
@@ -25,6 +27,9 @@ class Scoreboard(Turtle):
     def reset(self):
         if self.score > self.high_score:
             self.high_score = self.score
+            with open("data.txt", mode="w") as data:
+                data.write(f"{self.high_score}")
+
         self.score = 0
         self.update_scoreboard()
 
